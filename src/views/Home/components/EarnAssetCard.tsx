34ed5469ff20b10c2f5500184f623c6d17ffb89e
Mike <root@mikemamaril.com>
2021-04-27 01:04:26 +0800

    pools text

--- src/views/Home/components/EarnAssetCard.tsx
+++ src/views/Home/components/EarnAssetCard.tsx
@@ -22,8 +22,8 @@ const CardMidContent = styled(Heading).attrs({ size: 'xl' })`
 const EarnAssetCard = () => {
   const activeNonCakePools = pools.filter((pool) => !pool.isFinished && !pool.earningToken.symbol.includes('CAKE'))
   const latestPools: Pool[] = orderBy(activeNonCakePools, ['sortOrder', 'pid'], ['desc', 'desc']).slice(0, 3)
-  // Always include RICE
-  const assets = ['RICE', ...latestPools.map((pool) => pool.earningToken.symbol)].join(', ')
+  // Always include TeslaSafe
+  const assets = ['TeslaSafe', ...latestPools.map((pool) => pool.earningToken.symbol)].join(', ')
 
   return (
     <StyledFarmStakingCard>
