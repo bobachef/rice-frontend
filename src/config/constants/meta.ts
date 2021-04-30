6da6acb3b3dddc4d1caab4c426bddba7d283d2b5
Mike <root@mikemamaril.com>
2021-05-01 02:46:45 +0800

    ricefarm text replace

--- src/config/constants/meta.ts
+++ src/config/constants/meta.ts
@@ -1,16 +1,16 @@
 import { PageMeta } from './types'
 
 export const DEFAULT_META: PageMeta = {
-  title: 'PancakeSwap',
+  title: 'RiceFarm',
   description:
-    'The most popular AMM on BSC by user count! Earn CAKE through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by PancakeSwap), NFTs, and more, on a platform you can trust.',
+    'The most popular AMM on BSC by user count! Earn CAKE through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by RiceFarm), NFTs, and more, on a platform you can trust.',
   image: 'https://pancakeswap.finance/images/easter-battle.png',
 }
 
 export const customMeta: { [key: string]: PageMeta } = {
   '/competition': {
-    title: 'PancakeSwap Easter Battle',
-    description: 'Register now for the PancakeSwap Easter battle!',
+    title: 'RiceFarm Easter Battle',
+    description: 'Register now for the RiceFarm Easter battle!',
     image: 'https://pancakeswap.finance/images/easter-battle.png',
   },
 }
