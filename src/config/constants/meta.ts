ce7638bdd8d4af58b6e3cdfac4033d09ae215e4a
Boba Group <bobagroup@protonmail.com>
2021-05-25 19:55:31 -0700

    ft: meta update

--- src/config/constants/meta.ts
+++ src/config/constants/meta.ts
@@ -4,7 +4,7 @@ export const DEFAULT_META: PageMeta = {
   title: 'RiceFarm',
   description:
     'The most popular AMM on BSC by user count! Earn CAKE through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by RiceFarm), NFTs, and more, on a platform you can trust.',
-  image: 'https://pancakeswap.finance/images/easter-battle.png',
+  image: 'https://ricefarm.fi/images/logo.png', // todo: change to RICE logo
 }
 
 export const customMeta: { [key: string]: PageMeta } = {
